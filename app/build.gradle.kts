28dad8525cb3cdb906a4c1745e31c1f598ea6927
Ujjwal <Ujjwal22154010@akgec.ac.in>
2024-06-25 17:05:42 +0530

    bottom nav added

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.android.recipe"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.android.recipe"
@@ -32,6 +32,10 @@ android {
     }
     kotlinOptions {
         jvmTarget = "1.8"
+    }
+
+    buildFeatures{
+        viewBinding = true
     }
 }
 
--- app/src/main/java/com/android/recipe/MainActivity.kt
+++ app/src/main/java/com/android/recipe/MainActivity.kt
@@ -1,11 +1,52 @@
 package com.android.recipe
 
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.core.content.ContentProviderCompat.requireContext
+import androidx.core.content.ContextCompat
+import androidx.fragment.app.Fragment
+import com.android.recipe.databinding.ActivityMainBinding
+import com.android.recipe.fragment.Favourite
+import com.android.recipe.fragment.Home
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        replaceFrags(Home())
+        setStatusBarColor()
+
+        binding.bottomNavigationView.setOnItemSelectedListener {
+            when (it.itemId) {
+                R.id.home -> replaceFrags(Home())
+                R.id.favourite -> replaceFrags(Favourite())
+                else -> {
+
+                }
+            }
+            true
+        }
+    }
+
+    private fun replaceFrags(fragment: Fragment) {
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.frame_layout, fragment)
+        fragmentTransaction.commit()
+    }
+
+    private fun setStatusBarColor() {
+        window?.apply {
+            var statusBarColor = ContextCompat.getColor(this@MainActivity, R.color.white)
+            this.statusBarColor = statusBarColor
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
+            }
+        }
     }
 }
